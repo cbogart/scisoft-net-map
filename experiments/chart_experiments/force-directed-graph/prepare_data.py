a2294472a54ed82488d3d2123277eab584881077
Nikita Chepanov <nikita@chepanov.com>
2014-05-25 18:48:35 -0400

    updated

--- experiments/chart_experiments/force-directed-graph/prepare_data.py
+++ experiments/chart_experiments/force-directed-graph/prepare_data.py
@@ -5,25 +5,25 @@ import sys
 import math
 
 counter = 0
-threshold = 10
+lo, hi = 10, 100 
 links = []
 apps = {}
 
 def new_app(hash):
 		global counter
 		if hash not in apps:
-				apps[hash] = counter
-				counter+=1
+			apps[hash] = counter
+			counter+=1
 		return apps[hash]
 
 
 with open(sys.argv[1]) as f:
 	for line in f.readlines():
 		fields = line.split(";")
-		co, self = fields[0], fields[2]
-		print self
-		if co > threshold:
-			for i in range(3, len(fields)-1):
+		co, self = int(fields[0]), fields[2]
+		if lo < co  < hi:
+			print self, len(apps)
+			for i in range(3, min(len(fields)-1, 500)):
 				app,value = fields[i].split(":")
 				if app == self:
 						continue
@@ -31,8 +31,9 @@ with open(sys.argv[1]) as f:
 					"source": new_app(self),
 					"target": new_app(app),
 					"value" : int(math.log(float(value)))})
-		if len(links) > threshold*threshold:
-				break
+		if counter > 100:
+			break
+print counter
 nodes = []
 for a in apps:
 	nodes.append({"name":a, "group":1})
