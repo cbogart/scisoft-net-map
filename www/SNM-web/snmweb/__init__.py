7442d1904d9f1ecbf5ebd1c82263e4770fbb9f88
slava <tyumener@yandex.ru>
2014-07-03 15:21:56 -0400

    Change routing of application details

--- www/SNM-web/snmweb/__init__.py
+++ www/SNM-web/snmweb/__init__.py
@@ -11,7 +11,7 @@ def main(global_config, **settings):
     config.add_static_view(name='static', path='static')
 
     config.add_route('home', '/')
-    config.add_route('app_details', '/app_details/{name}')
+    config.add_route('app_details', '/application/{name}/usage')
     config.add_route('application', '/application/{name}')
     config.add_route('compare', '/compare')
     config.add_route('data-sources', '/data-sources')
