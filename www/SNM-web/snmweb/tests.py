3a80dd6e42fe080b3610a5d99e04d16dcc34bbfc
slava <tyumener@yandex.ru>
2014-07-08 12:48:51 -0400

    Add one unit and one funcitonal test

--- www/SNM-web/snmweb/tests.py
+++ www/SNM-web/snmweb/tests.py
@@ -10,8 +10,23 @@ class ViewTests(unittest.TestCase):
     def tearDown(self):
         testing.tearDown()
 
-    def test_my_view(self):
-        from .views import my_view
+    def test_view_home(self):
+        from .views import view_home
         request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'SNM-web')
+        response = view_home(request)
+        self.assertEquals(response["status"], "200 OK")
+
+
+class FunctionalTests(unittest.TestCase):
+    def setUp(self):
+        from snmweb import main
+        app = main({})
+        from webtest import TestApp
+        self.testapp = TestApp(app)
+
+    def tearDown(self):
+        del self.testapp
+
+    def test_root(self):
+        res = self.testapp.get("/", status=200)
+        self.assertTrue("Scientific Network Map" in res.body)
