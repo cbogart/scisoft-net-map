64ff0561b3da0cc601279d14a613d5fef269a6ab
slava <tyumener@yandex.ru>
2014-07-14 18:41:50 -0400

    Add some db queries to site

--- www/SNM-web/snmweb/__init__.py
+++ www/SNM-web/snmweb/__init__.py
@@ -2,6 +2,7 @@ from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
+
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
--- www/SNM-web/snmweb/views.py
+++ www/SNM-web/snmweb/views.py
@@ -1,5 +1,7 @@
 from pyramid.view import view_config
 from pyramid.response import Response
+from mongoengine import *
+from db_objects import *
 
 
 @view_config(route_name="overview", renderer='templates/overview.jinja2')
@@ -14,23 +16,31 @@ def view_home(request):
 
 @view_config(route_name="app_usage", renderer='templates/app_usage.jinja2')
 def view_app_usage(request):
-    return { "name": request.matchdict["name"]}
+    return {"name": request.matchdict["name"]}
+
 
 @view_config(route_name="app_used_with", renderer='templates/app_used_with.jinja2')
 def view_app_used_with(request):
-    return { "name": request.matchdict["name"]}
+    return {"name": request.matchdict["name"]}
+
 
 @view_config(route_name="application", renderer='templates/application.jinja2')
 def view_application(request):
-    return { "name": request.matchdict["name"]}
+    connect("snm-test")
+    name = request.matchdict["name"]
+    app = Application.objects(title=name)
+    return {"app": app}
+
 
 @view_config(route_name="compare", renderer='templates/compare.jinja2')
 def view_compare(request):
     return {"status": "200 OK"}
 
+
 @view_config(route_name="data-sources", renderer='templates/data-sources.jinja2')
 def view_data_sources(request):
     return {"status": "200 OK"}
+
 
 @view_config(route_name="browse", renderer='templates/browse.jinja2')
 def view_explore(request):
