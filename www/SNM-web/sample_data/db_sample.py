97c961a4c6b586dd473879dffd40e14ffec55fe3
Nikita Chepanov <nikita@chepanov.com>
2014-07-19 21:19:21 -0400

    Fix sample_db.py

--- www/SNM-web/sample_data/db_sample.py
+++ www/SNM-web/sample_data/db_sample.py
@@ -1,8 +1,10 @@
 from snmweb.db_objects import *
+import random
 from random import randrange
 import datetime
 from datetime import datetime as dt
 import json
+import os
 
 
 def erase_data():
@@ -13,18 +15,32 @@ def erase_data():
     for app in Application.objects:
         app.delete()
 
+def any_usage(usage_path, hash):
+    result = {}
+    for field in ["daily", "weekly", "monthly"]:
+        with open(os.path.join(usage_path, "group_by",
+                               "{}-{}.json".format(hash, field))) as f:
+            result[field] = json.load(f)["data"]
+    return result
 
-def load_data(filename="db_sample.json"):
+
+def load_data(filename="db_sample.json", usage_path="../../../scripts/data_extraction/over_time"):
     print("Loading information from {}".format(filename))
+    f = open(os.path.join(usage_path, "30-applications"))
+    hash_list = f.readlines()
     with open(filename) as f:
         data = json.load(f)
         for a in data:
             app = Application(**a)
+            hash = random.choice(hash_list)[:10]
             app.save()
             field_data = {}
             for field in ["daily", "weekly", "monthly"]:
+                usage = any_usage(usage_path, hash)
                 stat = []
-                for entry in a["usage_{}".format(field)]:
+                print app.title, field,10*"-"
+                for entry in usage[field]:
+                    print "\t", entry
                     stat.append(ByDateStat(**entry))
                 field_data[field] = stat
             usage = Usage(application = app, **field_data)
