61fa63d9a2952b1a5ce274836d3efe2c339c12ae
Chris Bogart <cbogartdenver@gmail.com>
2014-08-21 18:38:54 -0400

    Script to add web users and their passwords.

---
+++ scripts/data_extraction/web_users/setUsers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#
+# Authors:
+#  Chris Bogart, Nikita Chepanov, Biao "Leo" Ma, Svyatoslav "Slava" Kovtunenko
+#
+from passlib.apps import custom_app_context as pwd_context
+from pymongo import Connection
+
+def addUser(db, userid, password):
+    c = Connection()
+    c[db]["web_users"].save({
+	"userid": userid,
+	"password": pwd_context.encrypt(password) })
+
+if __name__ == "__main__":
+    addUser("snm-web", "guest", "gazebo52")
