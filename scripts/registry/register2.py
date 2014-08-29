67bc3e87f0563c6dbb9b08282d6dba904e47d92d
Production server <nchepano@andrew.cmu.edu>
2014-08-29 17:03:30 -0400

    Some files for handling live incoming TCP/IP data from R

---
+++ scripts/registry/register2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+#
+# Author:
+#  Chris Bogart
+#
+import json
+import sys
+import os
+import datetime
+import time
+import math
+import pdb
+import socket
+from pymongo import MongoClient, Connection
+from collections import defaultdict
+from datetime import date, timedelta
+from os import walk
+from datetime import datetime as dt
+from snmweb.db_objects import *
+from processLariatOnline import addOne
+
+def await():
+    c = Connection()
+    HOST = ''                 # Symbolic name meaning all available interfaces
+    PORT = 7779               # Arbitrary non-privileged port
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(3)
+    while 1:
+        conn, addr = s.accept()
+        print 'Connection from', addr
+        data = ""
+        while 1:
+            rcvd = conn.recv(4096)
+            if not rcvd: break
+            data = data + rcvd
+        conn.close()
+        register(c, data)
+        
+def register(c, data):
+    try:
+        rawrecords = c["snm-raw-records"]
+        record = json.loads(data)
+        rawrecords["lariat2"].save(record)
+        addOne(c, c["snm-r"], record)
+        print "Registered a usage!"
+    except Exception as e:
+        print "Error: " + str(e)
+
+
+
+if __name__ == "__main__":
+	await()
