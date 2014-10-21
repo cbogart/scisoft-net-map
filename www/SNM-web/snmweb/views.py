5571b2481899838891e5579ac97eb8899b6bb4ab
Chris Bogart <cbogartdenver@gmail.com>
2014-10-21 15:20:59 -0400

    Merge in other DSM change from R branch

--- www/SNM-web/snmweb/views.py
+++ www/SNM-web/snmweb/views.py
@@ -7,18 +7,27 @@ from db_objects import *
 from passlib.apps import custom_app_context as pwd_context
 from pyramid.view import forbidden_view_config
 
+def count_visits(request):
+    visits=int(request.cookies.get('visits', "0"))+1
+    request.response.set_cookie('visits', value=str(visits), max_age=1000000)
+    return(visits)
+
 @view_config(route_name="overview",
              renderer='templates/overview.jinja2')
 def view_overview(request):
-    return {"status": "200 OK"}
+    return {"status": "200 OK", "visits": count_visits(request)}
 
+@view_config(route_name="dsm",
+             renderer='templates/dsm.jinja2',
+             permission='view')
+def view_dsm(request):
+    return {"status": "200 OK", "visits": count_visits(request)}
 
 @view_config(route_name="home",
              renderer='templates/index.jinja2',
              permission='view')
 def view_home(request):
-    return {"status": "200 OK"}
-
+    return {"status": "200 OK", "visits": count_visits(request)}
 
 @view_config(route_name="app_usage",
              renderer='templates/app_usage.jinja2',
@@ -26,7 +35,7 @@ def view_home(request):
 def view_app_usage(request):
     name = request.matchdict["name"]
     app = Application.objects(title=name).first()
-    return {"app": app}
+    return {"app": app, "visits": count_visits(request)}
 
 
 @view_config(route_name="app_users",
@@ -35,7 +44,7 @@ def view_app_usage(request):
 def view_app_users(request):
     name = request.matchdict["name"]
     app = Application.objects(title=name).first()
-    return {"app": app}
+    return {"app": app, "visits": count_visits(request)}
 
 @view_config(route_name="app_pubs",
              renderer='templates/app_pubs.jinja2',
@@ -45,7 +54,7 @@ def view_app_pubs(request):
     app = Application.objects(title=name).first()
     publist = PubList.objects(application=app).first()
 
-    return {"app": app, "pubs": publist}
+    return {"app": app, "pubs": publist, "visits": count_visits(request)}
 
 @view_config(route_name="app_used_with",
              renderer='templates/app_used_with.jinja2',
@@ -53,8 +62,7 @@ def view_app_pubs(request):
 def view_app_used_with(request):
     name = request.matchdict["name"]
     app = Application.objects(title=name).first()
-    return {"app": app}
-
+    return {"app": app, "visits": count_visits(request)}
 
 @view_config(route_name="application",
              renderer='templates/application.jinja2',
@@ -62,24 +70,24 @@ def view_app_used_with(request):
 def view_application(request):
     name = request.matchdict["name"]
     app = Application.objects(title=name).first()
-    return {"app": app}
+    return {"app": app, "visits": count_visits(request)}
 
 
 @view_config(route_name="compare",
              renderer='templates/compare.jinja2',
              permission='view')
 def view_compare(request):
-    return {"status": "200 OK"}
+    return {"status": "200 OK", "visits": count_visits(request)}
 
 @view_config(route_name="about",
              renderer='templates/about.jinja2')
 def view_about(request):
-    return {"status": "200 OK"}
+    return {"status": "200 OK", "visits": count_visits(request)}
 
 @view_config(route_name="data_source",
              renderer='templates/data_source.jinja2')
 def view_data_source(request):
-    return {"status": "200 OK"}
+    return {"status": "200 OK", "visits": count_visits(request)}
 
 @forbidden_view_config()
 def view_forbidden(request):
@@ -114,4 +122,4 @@ def view_explore(request):
     order = request.params.get("order", "usage")
     query = request.params.get("query", "")
     apps = Application.objects(title__icontains=query).order_by(order)
-    return {"apps": apps}
+    return {"apps": apps, "visits": count_visits(request)}
